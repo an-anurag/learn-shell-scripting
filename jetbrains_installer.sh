33955f7581f5e8839835df032dbece5438943421
appyens <an.anurag@msn.com>
2020-02-25 21:07:14 +0530

    jetbrains_installer.sh updated

--- jetbrains_installer.sh
+++ jetbrains_installer.sh
@@ -12,21 +12,23 @@ echo "$CHOICE"
 pycharm_dir="pycharm*"
 webstorm_dir="WebStorm*"
 pycharm_url="https://data.services.jetbrains.com/products/download?code=PCP&platform=linuxAnaconda&_ga=2.141656369.246534920.1581577435-133917865.1578895696"
-file_name="pycharm-anaconda.tar.gz"
+webstorm_url="https://download.jetbrains.com/webstorm/WebStorm-2019.3.3.tar.gz?_ga=2.88742043.1611910552.1582538215-133917865.1578895696"
+pycharm_file_name="pycharm-anaconda.tar.gz"
+webstorm_file_name="webstorm.tar.gz"
 
 
 # Download package
 if [ "$CHOICE" == 1 ]; then
   echo "Downloading PyCharm..."
-  wget -O $file_name $pycharm_url
+  wget -O $pycharm_file_name $pycharm_url
   echo "Download finished"
   sleep 2s
   echo "Installing Pycharm"
   rm -rf /opt/$pycharm_dir
   rm -rf ~/.java/.userPrefs/jetbrains/pycharm
   rm -rf ~/PycharmProjects
-  tar -xvzf $file_name -C /opt/
-  rm $file_name
+  tar -xvzf $pycharm_file_name -C /opt/
+  rm $pycharm_file_name
   rm ~/Desktop/Pycharm.sh
   sleep 2s
   echo "Adding shortcut to desktop"
@@ -34,7 +36,20 @@ if [ "$CHOICE" == 1 ]; then
   echo '#!/bin/bash\nLOC=/opt/pycharm*/bin/\ncd $LOC\n./pycharm.sh' > ~/Desktop/PyCharm.sh
   chmod 777 ~Desktop/PyCharm.sh
 else
+  sleep 2s
   echo "Downloading Webstorm..."
-  wget "https://www.jetbrains.com/webstorm/download/download-thanks.html?platform=linux"
-  tar -xvzf $webstorm_dir -C /opt/
+  wget -O $webstorm_file_name $webstorm_url
+  echo "Download finished"
+  sleep 2s
+  echo "Installing Webstorm"
+  rm -rf /opt/$webstorm_dir
+  rm -rf ~/.java/.userPrefs/jetbrains/webstorm
+  tar -xvzf $webstorm_file_name -C /opt/
+  rm $webstorm_file_name
+  rm ~/Desktop/Pycharm.sh
+  sleep 2s
+  echo "Adding shortcut to desktop"
+  touch WebStorm.sh
+  echo '#!/bin/bash\nLOC=/opt/webstorm*/bin/\ncd $LOC\n./pycharm.sh' > ~/Desktop/PyCharm.sh
+  chmod 777 ~Desktop/WebStorm.sh
 fi
