0a2e842d46df79d5fb7322a904a82b0bead02106
appyens <an.anurag@msn.com>
2020-02-26 01:17:03 +0530

    installer updated

--- jetbrains_installer.sh
+++ jetbrains_installer.sh
@@ -24,17 +24,17 @@ if [ "$CHOICE" == 1 ]; then
   echo "Download finished"
   sleep 2s
   echo "Installing Pycharm"
-  rm -rf /opt/$pycharm_dir
-  rm -rf ~/.java/.userPrefs/jetbrains/pycharm
-  rm -rf ~/PycharmProjects
-  tar -xvzf $pycharm_file_name -C /opt/
-  rm $pycharm_file_name
-  rm ~/Desktop/Pycharm.sh
+  sudo rm -rf /opt/$pycharm_dir
+  sudo rm -rf ~/.java/.userPrefs/jetbrains/pycharm
+  sudo rm -rf ~/PycharmProjects
+  sudo tar -xvzf $pycharm_file_name -C /opt/
+#  rm $pycharm_file_name
+  sudo rm ~/Desktop/Pycharm.sh
   sleep 2s
   echo "Adding shortcut to desktop"
-  touch PyCharm.sh
+  touch ~/Desktop/Pycharm.sh
   echo '#!/bin/bash\nLOC=/opt/pycharm*/bin/\ncd $LOC\n./pycharm.sh' > ~/Desktop/PyCharm.sh
-  chmod 777 ~Desktop/PyCharm.sh
+  sudo chmod 777 ~/Desktop/PyCharm.sh
 else
   sleep 2s
   echo "Downloading Webstorm..."
@@ -42,14 +42,15 @@ else
   echo "Download finished"
   sleep 2s
   echo "Installing Webstorm"
-  rm -rf /opt/$webstorm_dir
-  rm -rf ~/.java/.userPrefs/jetbrains/webstorm
-  tar -xvzf $webstorm_file_name -C /opt/
-  rm $webstorm_file_name
-  rm ~/Desktop/Pycharm.sh
+  sudo rm -rf /opt/$webstorm_dir
+  sudo rm -rf ~/.java/.userPrefs/jetbrains/webstorm
+  sudo tar -xvzf $webstorm_file_name -C /opt/
+#  rm $webstorm_file_name
+  sudo rm ~/Desktop/Pycharm.sh
   sleep 2s
   echo "Adding shortcut to desktop"
-  touch WebStorm.sh
-  echo '#!/bin/bash\nLOC=/opt/webstorm*/bin/\ncd $LOC\n./pycharm.sh' > ~/Desktop/PyCharm.sh
-  chmod 777 ~Desktop/WebStorm.sh
+  touch ~/Desktop/WebStorm.sh
+  echo '#!/bin/bash\nLOC=/opt/webstorm*/bin/\ncd $LOC\n./pycharm.sh' > ~/Desktop/WebStorm.sh
+  sudo chmod 777 ~/Desktop/WebStorm.sh
 fi
+
