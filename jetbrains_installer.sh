71219ea609c7be6db435a4c75e6b866f7c10e0f0
appyens <an.anurag@msn.com>
2020-02-26 02:15:38 +0530

    installer updated

--- jetbrains_installer.sh
+++ jetbrains_installer.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-echo "###############################"
-echo "Welcome to JetBrains Installer"
+echo "###################################"
+echo "WELCOME TO JETBRAINS IDE INSTALLER"
+echo "###################################"
 sleep 2s
 echo "Choose IDE to Install"
 echo "1. PyCharm Professional"
@@ -16,8 +17,32 @@ webstorm_url="https://download.jetbrains.com/webstorm/WebStorm-2019.3.3.tar.gz?_
 pycharm_file_name="pycharm-anaconda.tar.gz"
 webstorm_file_name="webstorm.tar.gz"
 
+function install_ide() {
+  url=$1
+  file_name=$2
+  dir_name=$3
 
-# Download package
+  echo "Downloading %s" %file_name
+  wget -O $file_name $url
+  echo "Download finished"
+  sleep 2s
+  echo "Installing %s" %file_name
+  sudo rm -rf /opt/$dir_name
+  sudo rm -rf ~/.java/.userPrefs/jetbrains/$dir_name
+  sudo rm -rf ~/PycharmProjects
+  sudo rm -rf ~/.PyCharm*
+  sudo tar -xvzf $pycharm_file_name -C /opt/
+  sudo rm ~/Desktop/PyCharm.sh
+  sleep 2s
+  echo "Creating shortcut on desktop..."
+  touch ~/Desktop/PyCharm.sh
+  echo -e '#!/bin/bash \nLOC=/opt/pycharm*/bin/ \ncd $LOC \n./pycharm.sh \n' > ~/Desktop/PyCharm.sh
+  sudo chmod 777 ~/Desktop/PyCharm.sh
+  rm $pycharm_file_name
+  sleep 2s
+  echo "Pycharm installed successfully"
+}
+
 if [ "$CHOICE" == 1 ]; then
   echo "Downloading PyCharm..."
   wget -O $pycharm_file_name $pycharm_url
@@ -29,13 +54,14 @@ if [ "$CHOICE" == 1 ]; then
   sudo rm -rf ~/PycharmProjects
   sudo rm -rf ~/.PyCharm*
   sudo tar -xvzf $pycharm_file_name -C /opt/
-#  rm $pycharm_file_name
-  sudo rm ~/Desktop/Pycharm.sh
+  sudo rm ~/Desktop/PyCharm.sh
   sleep 2s
-  echo "Adding shortcut to desktop"
+  echo "Creating shortcut on desktop..."
   touch ~/Desktop/PyCharm.sh
   echo -e '#!/bin/bash \nLOC=/opt/pycharm*/bin/ \ncd $LOC \n./pycharm.sh \n' > ~/Desktop/PyCharm.sh
   sudo chmod 777 ~/Desktop/PyCharm.sh
+  rm $pycharm_file_name
+  sleep 2s
   echo "Pycharm installed successfully"
 else
   sleep 2s
@@ -44,16 +70,18 @@ else
   echo "Download finished"
   sleep 2s
   echo "Installing Webstorm"
-  sudo rm -rf /opt/$webstorm_dir && rm -rf ~/.java/.userPrefs/jetbrains/webstorm
+  sudo rm -rf /opt/$webstorm_dir
+  sudo rm -rf ~/.java/.userPrefs/jetbrains/webstorm
   sudo rm -rf ~/.WebStorm*
   sudo tar -xvzf $webstorm_file_name -C /opt/
-#  rm $webstorm_file_name
   sudo rm ~/Desktop/WebStorm.sh
   sleep 2s
-  echo "Adding shortcut to desktop"
+  echo "Creating shortcut on desktop..."
   touch ~/Desktop/WebStorm.sh
   echo '#!/bin/bash \nLOC=/opt/webstorm*/bin/ \ncd $LOC \n./WebStorm.sh\n' > ~/Desktop/WebStorm.sh
   sudo chmod 777 ~/Desktop/WebStorm.sh
+  rm $webstorm_file_name
+  sleep 2s
   echo "WebStorm installed successfully"
 fi
 
